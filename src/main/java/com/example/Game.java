28b57f4ab9c53607d52ec95a736394c72db72e04
Kazumasa Fujita <fujita.kazumasa.testdesu@gmail.com>
2021-05-20 10:18:23 +0900

    add Game class

---
+++ src/main/java/com/example/Game.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Game {
+
+    //private final Enemy enemy;
+    //private final Player player;
+    //private final
+
+
+    public Game (){ // Enemy enemy, Player player){
+        //this.enemy = enemy;
+        //this.player = player;
+        System.out.println("Game class here");
+    }
+}
